a8160b2b7d2d87971580ff4ac33003ec4a53afb7
PanjeGogo <3lagu.id@gmail.com>
2026-09-10 21:09:52 +0700

    Add getgold loader reference

---
+++ getgold_loader.lua
@@ -0,0 +1,10 @@
+-- Source loader reference
+-- Original:
+-- loadstring(game:HttpGet("https://getgold.cc"))()
+--
+-- The URL above is a remote script endpoint. This repository stores the
+-- reference URL for analysis; it does not execute the remote code.
+
+local SOURCE_URL = "https://getgold.cc"
+
+print("Remote source:", SOURCE_URL)
